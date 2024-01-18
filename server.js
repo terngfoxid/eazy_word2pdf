cac7348493d65d138815e3fba121420c1bbb8816
terngfoxid <terngfoxid@live.jp>
2024-01-18 16:34:59 +0700

    refactor(server.js):move service to index

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ var multer = require('multer')
 const path = require('path');
 const fs = require('fs').promises;
 var convertWordFiles = require('convert-multiple-files-ul');
+var cors = require('cors')
+
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
 
 var app = express()
+app.use(cors(corsOptions));
 app.use(multer({ dest: './uploads/' }).single('file'));
 
 //ui route
@@ -17,32 +24,19 @@ app.get('/word2pdf_ui', function (req, res) {
 })
 
 //function route
-app.post('/upload', function (req, res) {
-    //req.file.originalname
-    //req.file.mimetype
-    const newPath = './uploads/' + req.file.originalname
-    fs.rename(req.file.path, newPath, function (err) {
-        if (err) {
-            console.log('ERROR: ' + err);
-            res.sendStatus(400)
-        }
-    });
-    res.sendStatus(200)
-})
-
-app.post('/word2pdf', async function (req, res) {
-    let fileName =[]
+app.post('/', async function (req, res) {
+    let fileName = []
     switch (req.file.mimetype) {
         case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-            fileName = req.file.originalname.split(".docx")
+            fileName = req.file.originalname.split(".docx");
             break;
         case "application/msword":
-            fileName = req.file.originalname.split(".doc")
+            fileName = req.file.originalname.split(".doc");
             break;
-        default: res.sendStatus(400);
+        default: ;
     }
     if (fileName.length < 1) {
-        res.sendStatus(400)
+        fileName[0] = req.file.filename;
     }
 
     const newPath = './uploads/' + req.file.originalname
@@ -62,25 +56,76 @@ app.post('/word2pdf', async function (req, res) {
     await fs.writeFile(outputFilePath, pdfBuf);*/
     try {
         const pathOutput = await convertWordFiles.convertWordFiles(sourceFilePath, 'pdf', outputFileDir);
-        res.sendFile(pathOutput);
+        res.sendFile(pathOutput)
     } catch (err) {
         console.log(err);
-        res.sendStatus(500)
+        res.sendStatus(500);
     }
     
     try {
         const files = await fs.readdir('./uploads');
-    
+
         const deleteFilePromises = files.map(file =>
-          fs.unlink(path.join('./uploads', file)),
+            fs.unlink(path.join('./uploads', file)),
         );
-    
+
         await Promise.all(deleteFilePromises);
-      } catch (err) {
+    } catch (err) {
         console.log(err);
-      }
+    }
 })
 
-app.listen(8087, function () {
-    console.log('App running on port 8087')
+app.post('/word2pdf', async function (req, res) {
+    let fileName = []
+    switch (req.file.mimetype) {
+        case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+            fileName = req.file.originalname.split(".docx");
+            break;
+        case "application/msword":
+            fileName = req.file.originalname.split(".doc");
+            break;
+        default: ;
+    }
+    if (fileName.length < 1) {
+        fileName[0] = req.file.filename;
+    }
+
+    const newPath = './uploads/' + req.file.originalname
+    await fs.rename(req.file.path, newPath, function (err) {
+        if (err) {
+            console.log('ERROR: ' + err);
+            res.sendStatus(400)
+        }
+    });
+
+    const sourceFilePath = newPath;
+    //const outputFilePath = path.resolve('./uploads/' + fileName[0] + '.pdf');
+    const outputFileDir = path.resolve('./uploads/');
+
+    /*const docxBuf = await fs.readFile(sourceFilePath);
+    let pdfBuf = await libre.convertAsync(docxBuf, '.pdf', undefined);
+    await fs.writeFile(outputFilePath, pdfBuf);*/
+    try {
+        const pathOutput = await convertWordFiles.convertWordFiles(sourceFilePath, 'pdf', outputFileDir);
+        res.sendFile(pathOutput)
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+    
+    try {
+        const files = await fs.readdir('./uploads');
+
+        const deleteFilePromises = files.map(file =>
+            fs.unlink(path.join('./uploads', file)),
+        );
+
+        await Promise.all(deleteFilePromises);
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+app.listen(8096, function () {
+    console.log('App running on port 8096')
 })
